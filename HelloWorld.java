e2f58f364f5a3250552294ec52807aabbda82d10
Lina Breu <65247475+linabreu@users.noreply.github.com>
2020-05-13 16:44:12 -0700

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,19 @@
+/*
+Lina Breunlin
+September 9. 2019
+CIS 163AA Section 20460
+Display Hello World greeting on screen
+ */
+package helloworld;
+
+
+public class HelloWorld 
+{
+
+    
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello, World!");
+    }
+    
+}
